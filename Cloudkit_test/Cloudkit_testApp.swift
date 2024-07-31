b212a8448ab9b5a95860544e7ac46d73e2330d9c
Hans Jonathan Zebua <hans.zebua@icloud.com>
2024-07-31 22:14:48 +0700

    Initial Commit

---
+++ Cloudkit_test/Cloudkit_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cloudkit_testApp.swift
+//  Cloudkit_test
+//
+//  Created by Hans Zebua on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Cloudkit_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
